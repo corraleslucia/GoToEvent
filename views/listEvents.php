bfc15654e0415797717155287ba71762bc96bef5
Carrizo Santiago <santiicaarrizo@gmail.com>
2018-11-01 02:00:43 -0300

    agregado boton <a> a los eventos

--- views/listEvents.php
+++ views/listEvents.php
@@ -11,8 +11,8 @@ include(ROOT.'views/navAdmin.php');
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Eventos</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="<?php echo BASE ?>css/normalize.css">
-  <link rel="stylesheet" type="text/css" media="screen" href="<?php echo BASE ?>css/style.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href= "<?php echo BASE ?>/css/normalize.css">
+  <link rel="stylesheet" type="text/css" media="screen" href="<?php echo BASE ?>/css/style.css" />
 
 </head>
 <body>
@@ -24,22 +24,28 @@ include(ROOT.'views/navAdmin.php');
         ?>
 
         <div class="event">
-            <h4>Ricky Martin vuelve con todo</h4>
-            <div class="p-listev-art"><p>Artista: Ricky Martin</p></div>
-            <div class="p-listev-plc"><p>Lugar: Polideportivo Mar del Plata</p></div>
-            <div class="p-listev-date"><p>10/12/2018</p></div>
+            <a class="link-divs" href="">
+                <h4>Ricky Martin vuelve con todo</h4>
+                <div class="p-listev-art"><p>Artista: Ricky Martin</p></div>
+                <div class="p-listev-plc"><p>Lugar: Polideportivo Mar del Plata</p></div>
+                <div class="p-listev-date"><p>10/12/2018</p></div>
+            </a>
         </div>
         <div class="event">
-            <h4>Lollapalooza</h4>
-            <div class="p-listev-art"><p>Artista: Lali Esposito, Daddy Yankee, The Beatles, Bob Marley, No te va gustar</p></div>
-            <div class="p-listev-plc"><p>Lugar: Tecnopolis</p></div>
-            <div class="p-listev-date"><p>17/05/2019</p></div>
+            <a class="link-divs" href="">
+                <h4>Lollapalooza</h4>
+                <div class="p-listev-art"><p>Artista: Lali Esposito, Daddy Yankee, The Beatles, Bob Marley, No te va gustar</p></div>
+                <div class="p-listev-plc"><p>Lugar: Tecnopolis</p></div>
+                <div class="p-listev-date"><p>17/05/2019</p></div>
+            </a>
         </div>
         <div class="event">
-            <h4>Arjona en Gran Rex</h4>
-            <div class="p-listev-art"><p>Artista: Ricardo Arjona</p></div>
-            <div class="p-listev-plc"><p>Lugar: Teatro Gran Rex</p></div>
-            <div class="p-listev-date"><p>1/11/2018</p></div>
+            <a class="link-divs" href="">
+                <h4>Arjona en Gran Rex</h4>
+                <div class="p-listev-art"><p>Artista: Ricardo Arjona</p></div>
+                <div class="p-listev-plc"><p>Lugar: Teatro Gran Rex</p></div>
+                <div class="p-listev-date"><p>1/11/2018</p></div>
+            </a>
         </div>
     </div>
 
