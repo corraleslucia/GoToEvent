8e92c132fb546854c76ddc8ca0b449f0453356f2
Carrizo Santiago <santiicaarrizo@gmail.com>
2018-11-01 02:59:58 -0300

    merged

--- views/listEvents.php
+++ views/listEvents.php
@@ -29,35 +29,9 @@ include(ROOT.'views/navAdmin.php');
                 }
         ?>
 
-<<<<<<< HEAD
-        <div class="event">
-            <a class="link-divs" href="">
-                <h4>Ricky Martin vuelve con todo</h4>
-                <div class="p-listev-art"><p>Artista: Ricky Martin</p></div>
-                <div class="p-listev-plc"><p>Lugar: Polideportivo Mar del Plata</p></div>
-                <div class="p-listev-date"><p>10/12/2018</p></div>
-            </a>
-        </div>
-        <div class="event">
-            <a class="link-divs" href="">
-                <h4>Lollapalooza</h4>
-                <div class="p-listev-art"><p>Artista: Lali Esposito, Daddy Yankee, The Beatles, Bob Marley, No te va gustar</p></div>
-                <div class="p-listev-plc"><p>Lugar: Tecnopolis</p></div>
-                <div class="p-listev-date"><p>17/05/2019</p></div>
-            </a>
-        </div>
-        <div class="event">
-            <a class="link-divs" href="">
-                <h4>Arjona en Gran Rex</h4>
-                <div class="p-listev-art"><p>Artista: Ricardo Arjona</p></div>
-                <div class="p-listev-plc"><p>Lugar: Teatro Gran Rex</p></div>
-                <div class="p-listev-date"><p>1/11/2018</p></div>
-            </a>
-=======
             <div class="event">
             <a href="<?= BASE ?>event/showEventDetails/<?php echo $value->getId()?>"> <h4><?php echo $value->getDescription()?></h4> </a>
             <div class="p-listev-art"><p>Categoria:<?php echo $_categoryName?></p></div>
->>>>>>> master
         </div>
     <?php } ?>
     </div>
