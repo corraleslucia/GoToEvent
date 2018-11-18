7956fceb9b50cfcfc9e478749174e40d6ed6bfdc
Carrizo Santiago <santiicaarrizo@gmail.com>
2018-11-18 01:23:31 -0300

    view cart

---
+++ views/viewCart.php
@@ -0,0 +1,38 @@
+<?php
+namespace views;
+include(ROOT.'views/headerAdmin.php');
+include(ROOT.'views/navAdmin.php');
+ ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Eventos</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href= "<?php echo BASE ?>/css/normalize.css">
+  <link rel="stylesheet" type="text/css" media="screen" href="<?php echo BASE ?>/css/style.css" />
+
+</head>
+<body>
+  <section class="content">
+    <h3>EVENTOS</h3>
+
+
+    <div class="container">
+        <div class="element">  
+                <div class="p-listev-art">
+                    <p style="font-size:22px"><b>Description</b></p>
+                </div>
+                <div class="p-listev-art">
+                    <p>Mas cosas</p>
+                </div>
+                <button>Eliminar del carrito</button>
+                <button>Comprar</button>
+        </div>
+    </div>
+
+  </section>
+</body>
+</html>
